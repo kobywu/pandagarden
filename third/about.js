085d1599acdbcdcd9b27bcaf7b4219b1879d0a7a
GutierrezLiliana <liliana_gutierrez@davincischools.org>
2019-04-03 13:56:55 -0700

    Update about.js

--- third/about.js
+++ third/about.js
@@ -2,6 +2,7 @@ window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+var x = document.getElementById("navbar");
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
