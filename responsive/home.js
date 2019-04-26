3b0c7bda02b75528d3bdfc8ceb5331f5f0dbc9af
Koby <koby_wu@davincischools.org>
2019-04-25 21:26:11 -0700

    Add files via upload

--- responsive/home.js
+++ responsive/home.js
@@ -64,36 +64,3 @@
 	
 	
 })();
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("centered_nav");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-function topbar() {
-    var x = document.getElementById("centered_nav");
-    if (x.className === "rc_nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "rc_nav";
-    }
-}
-
-function hover(el){
-	el.classList.add("hovereffect");
-}
-function nohover(el){
-	el.classList.remove("hovereffect");
-}
-function tabhover(el){
-	el.classList.add("tabhover");
-}
-function none(el){
-	el.classList.remove("tabhover");
-}
