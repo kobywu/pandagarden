5263b2a56d8fd17cc33d6f43123238c539d38087
GutierrezLiliana <liliana_gutierrez@student.davincischools.org>
2019-05-03 14:59:21 -0700

    Update home.js

--- responsive/home.js
+++ responsive/home.js
@@ -38,7 +38,7 @@
 				}
 				self._slideTo( self.index );
 				
-			}, 5000);
+			}, 3000);
 		},
 		stopStart: function() {
 			var self = this;
@@ -64,3 +64,14 @@
 	
 	
 })();
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
